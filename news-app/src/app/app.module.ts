b3c0bd809adb358b244ad853a6485ac20d8128f3
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2024-02-28 00:38:00 +0300

    feat: add ngrx

--- news-app/src/app/app.module.ts
+++ news-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -7,7 +9,14 @@ import { PostModule } from './post/post.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, PostModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    PostModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- news-app/src/app/post/post.module.ts
+++ news-app/src/app/post/post.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { PostRoutingModule } from './post-routing.module';
 import { PostItemComponent } from './components/post-item/post-item.component';
 import { DetailedPageComponent } from './pages/detailed-page/detailed-page.component';
 import { PostListPageComponent } from './pages/post-list-page/post-list-page.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
+import { postReducer } from './reducers/post.reducer';
+import { PostEffects } from './effects/post.effects';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,13 @@ import { UserPageComponent } from './pages/user-page/user-page.component';
     CommentComponent,
     UserPageComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule, PostRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    PostRoutingModule,
+    StoreModule.forFeature('posts', postReducer),
+    EffectsModule.forFeature([PostEffects]),
+  ],
   exports: [DetailedPageComponent],
 })
 export class PostModule {}
